49e66fcccbcf9e4a53d1383924e16501a0328b65
zhangxiangyu <zhangxiangyu@yuewen.com>
2018-07-28 11:33:10 +0800

    777

--- progressbar/app/src/main/java/com/example/zhangxiangyu/progressbar/MainActivity.java
+++ progressbar/app/src/main/java/com/example/zhangxiangyu/progressbar/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     EditText editText;
+    ImageView imageView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
