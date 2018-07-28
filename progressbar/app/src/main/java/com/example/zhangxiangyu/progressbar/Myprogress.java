7ce84c5b67f732c3e420821cb0c05f54f9da1d62
zhangxiangyu <zhangxiangyu@yuewen.com>
2018-07-28 10:56:25 +0800

    aa

---
+++ progressbar/app/src/main/java/com/example/zhangxiangyu/progressbar/Myprogress.java
@@ -0,0 +1,27 @@
+package com.example.zhangxiangyu.progressbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+public class Myprogress extends ProgressBar {
+    public Myprogress(Context context) {
+        super(context);
+    }
+
+    public Myprogress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Myprogress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        canvas.rotate(-90);
+        canvas.translate(-getHeight(),0);
+        super.onDraw(canvas);
+    }
+}
