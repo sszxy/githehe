5fc569598a8866c86714846cf33bdbbf287989f2
zhangxiangyu <zhangxiangyu@yuewen.com>
2018-07-28 12:49:57 +0800

    tijiaofenzhi1

--- progressbar/app/src/main/java/com/example/zhangxiangyu/progressbar/MainActivity.java
+++ progressbar/app/src/main/java/com/example/zhangxiangyu/progressbar/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     ImageView imageView6;
-    ImageView imageView7;
+    ImageView imageView8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
